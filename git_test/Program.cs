6c342a0fedb30b6a4d080e34391f59adb0361ad0
Pavel Tkalenko <invisiblenemy@yandex.ru>
2018-06-07 07:32:45 +0300

    добавлена функция

--- git_test/Program.cs
+++ git_test/Program.cs
@@ -8,9 +8,15 @@ namespace git_test
 {
     class Program
     {
+        static int Sum(int a, int b)
+        {
+            return a + b;
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
+            int c = Sum(2, 1);
+            Console.WriteLine($"{c}");
         }
     }
 }
